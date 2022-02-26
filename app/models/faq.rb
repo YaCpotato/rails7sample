8020067b32493804ed510fb05b721b62ba8d16d0
YaCpotato <yasshisshy@gmail.com>
2022-02-26 18:51:30 +0900

    モデルカラムリセット

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,5 +1,4 @@
 class Faq < ApplicationRecord
   has_many :comments
   has_many :categories
-  enum rate:    [ :good, :normal, :bad ]
 end
--- db/migrate/20220131042839_create_faqs_and_comments.rb
+++ db/migrate/20220131042839_create_faqs_and_comments.rb
@@ -2,17 +2,15 @@ class CreateFaqsAndComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.belongs_to :user
+      t.belongs_to :users
       t.timestamps
     end
 
     create_table :faqs do |t|
       t.text :question_text
       t.text :answer
-      t.belongs_to :large_category
-      t.belongs_to :medium_category
-      t.belongs_to :small_category
-      t.belongs_to :comment
+      t.belongs_to :categories
+      t.belongs_to :comments
       t.timestamps
     end
 
